d1f0b99d2f404fc893317c22cb105ec68f5b0655
LidaGold <50148896+LidaGold@users.noreply.github.com>
2020-05-17 16:48:53 -0600

    Modified Connection string

--- travel-experts/TravelExperts.Team1.WebApp/Models/TravelExpertsContext.cs
+++ travel-experts/TravelExperts.Team1.WebApp/Models/TravelExpertsContext.cs
@@ -42,7 +42,7 @@ namespace TravelExperts.Team1.WebApp.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=TravelExperts_Team1;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TravelExperts_Team1;Integrated Security=True");
             }
         }
 
