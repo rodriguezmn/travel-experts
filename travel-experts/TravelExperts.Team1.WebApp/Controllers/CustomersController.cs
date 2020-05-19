0a88975f1685b40c3c417b60e02407d1c0fb80e4
Karim Khan <kakhan87@gmail.com>
2020-05-19 17:07:12 -0600

    updated registration page

--- travel-experts/TravelExperts.Team1.WebApp/Controllers/CustomersController.cs
+++ travel-experts/TravelExperts.Team1.WebApp/Controllers/CustomersController.cs
@@ -56,15 +56,15 @@ namespace TravelExperts.Team1.WebApp.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CustomerId,CustFirstName,CustLastName,CustAddress,CustCity,CustProv,CustPostal,CustCountry,CustHomePhone,CustBusPhone,CustEmail,AgentId")] Customers customers)
+        public async Task<IActionResult> Create([Bind("CustomerId,CustFirstName,CustLastName,CustAddress,CustCity,CustProv,CustPostal,CustCountry,CustHomePhone,CustBusPhone,CustEmail,Username,Password,AgentId")] Customers customers)
         {
             if (ModelState.IsValid)
             {
                 _context.Add(customers);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Index","BookingDetails");
             }
-            ViewData["AgentId"] = new SelectList(_context.Agents, "AgentId", "AgentId", customers.AgentId);
+            //ViewData["AgentId"] = new SelectList(_context.Agents, "AgentId", "AgentId", customers.AgentId);
             return View(customers);
         }
 
@@ -90,7 +90,7 @@ namespace TravelExperts.Team1.WebApp.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("CustomerId,CustFirstName,CustLastName,CustAddress,CustCity,CustProv,CustPostal,CustCountry,CustHomePhone,CustBusPhone,CustEmail,AgentId")] Customers customers)
+        public async Task<IActionResult> Edit(int id, [Bind("CustomerId,CustFirstName,CustLastName,CustAddress,CustCity,CustProv,CustPostal,CustCountry,CustHomePhone,CustBusPhone,CustEmail,Username,Password,AgentId")] Customers customers)
         {
             if (id != customers.CustomerId)
             {
--- travel-experts/TravelExperts.Team1.WebApp/Models/Customers.cs
+++ travel-experts/TravelExperts.Team1.WebApp/Models/Customers.cs
@@ -17,39 +17,58 @@ namespace TravelExperts.Team1.WebApp.Models
         [Key]
         public int CustomerId { get; set; }
         [Required]
+        [Display(Name = "First Name")]
         [StringLength(25)]
         public string CustFirstName { get; set; }
         [Required]
+        [Display(Name = "Last Name")]
         [StringLength(25)]
         public string CustLastName { get; set; }
         [Required]
+        [Display(Name = "Address")]
         [StringLength(75)]
         public string CustAddress { get; set; }
         [Required]
+        [Display(Name = "City")]
         [StringLength(50)]
         public string CustCity { get; set; }
         [Required]
+        [Display(Name = "Province")]
         [StringLength(2)]
         public string CustProv { get; set; }
         [Required]
+        [RegularExpression(@"([A-Z,0-9,A-Z]{3})[ ]([0-9,A-Z,0-9]{3})",
+                ErrorMessage = "Entered postal code is not valid.")]
+        [Display(Name = "Postal Code")]
         [StringLength(7)]
         public string CustPostal { get; set; }
+        [Display(Name = "Country")]
         [StringLength(25)]
         public string CustCountry { get; set; }
         [StringLength(20)]
         public string CustHomePhone { get; set; }
         [Required]
+        [RegularExpression(@"([0-9]{3})[-]([0-9]{3})[-]([0-9]{4})",
+            ErrorMessage = "Entered phone format is not valid.")]
+        [Display(Name = "Phone Number")]
         [StringLength(20)]
         public string CustBusPhone { get; set; }
         [Required]
+        [Display(Name = "Email")]
         [StringLength(50)]
         public string CustEmail { get; set; }
         [Required]
         [StringLength(16)]
         public string Username { get; set; }
         [Required]
+        [Display(Name = "Password")]
         [StringLength(16)]
         public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
         public int? AgentId { get; set; }
 
         [ForeignKey(nameof(AgentId))]
