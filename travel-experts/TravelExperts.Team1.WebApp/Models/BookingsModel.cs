92dcc402d92070003eda9ca6078ff6b61af554ba
Irada Shamilova <isam007@gmail.com>
2020-05-19 15:01:28 -0600

    Created Bookings controller and View with joined Bookings and BookingDetails tables to display CustomerID. Yay!!!

---
+++ travel-experts/TravelExperts.Team1.WebApp/Models/BookingsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelExperts.Team1.WebApp.Models
+{
+    public class BookingsModel
+    {
+        public int BookingId { get; set; }
+        public int CustomerId { get; set; }
+        [DisplayName("Trip Starts")] public DateTime? TripStart { get; set; }
+
+        [DisplayName("Trip Ends")] public DateTime? TripEnd { get; set; }
+
+        public string Destination { get; set; }
+        public decimal? BasePrice { get; set; }
+        public decimal? AgencyCommission { get; set; }
+        public string Description { get; set; }
+    }
+}
