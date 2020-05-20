31ae97988de002d6dedbe63df1f00e35d0e6ccc2
Irada Shamilova <isam007@gmail.com>
2020-05-20 00:24:53 -0600

    Renamed Total Header on Booking page to fit in.

--- travel-experts/TravelExperts.Team1.WebApp/Controllers/BookingsController.cs
+++ travel-experts/TravelExperts.Team1.WebApp/Controllers/BookingsController.cs
@@ -5,42 +5,18 @@ using System.Threading.Tasks;
 using System.Xml.Schema;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using TravelExperts.Team1.WebApp.Managers;
 using TravelExperts.Team1.WebApp.Models;
 
 namespace TravelExperts.Team1.WebApp.Controllers
 {
     public class BookingsController : Controller
     {
-        public static List<BookingDetails> GetAllBookingDetails()
-        {
-            var context = new TravelExpertsContext();
-            var listOfBookingDetails = context.BookingDetails.Include(b => b.Booking);
-            return listOfBookingDetails.ToList();
-        }
-
-        public List<BookingDetails> GetAllByBookingId(int id)
-        {
-            var context = new TravelExpertsContext();
-            var filteredList = context.BookingDetails.
-                Where(bid => bid.BookingId == id).
-                Include(b => b.Booking).ToList();
-            return filteredList;
-        }
-
-        // Karim: To be revisted
-        //public List<Bookings> GetAllByBCustomerId(int id)
-        //{
-        //    var context = new TravelExpertsContext();
-        //    var listOfBookingDetails = context.BookingDetails.
-        //    Include(b => b.Booking).
-        //    Where(cid => cid.CustomerId == id);
-        //    return listOfBookingDetails.ToList();
-        //}
-
+        
         public ActionResult Index()
         {
-            var customerBookings = GetAllBookingDetails();
-            var viewModels = customerBookings.Select(a => new BookingsModel
+            var customerBookings = BookingManager.GetAllBookingDetails();
+            var viewModels = customerBookings.Select(a => new BookingsViewModel
             {
                 BookingId = Convert.ToInt32(a.BookingId),
                 CustomerId = Convert.ToInt32(a.Booking.CustomerId),
---
+++ travel-experts/TravelExperts.Team1.WebApp/Managers/BookingManager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelExperts.Team1.WebApp.Models;
+
+namespace TravelExperts.Team1.WebApp.Managers
+{
+    public class BookingManager
+    {
+        public static List<BookingDetails> GetAllBookingDetails()
+        {
+            var context = new TravelExpertsContext();
+            var listOfBookingDetails = context.BookingDetails.Include(b => b.Booking);
+            return listOfBookingDetails.ToList();
+        }
+
+        public static List<BookingDetails> GetAllByBookingId(int id)
+        {
+            var context = new TravelExpertsContext();
+            var filteredList = context.BookingDetails.
+                Where(bid => bid.BookingId == id).
+                Include(b => b.Booking).ToList();
+            return filteredList;
+        }
+
+        // Karim: To be revisted
+        //public List<Bookings> GetAllByBCustomerId(int id)
+        //{
+        //    var context = new TravelExpertsContext();
+        //    var listOfBookingDetails = context.BookingDetails.
+        //    Include(b => b.Booking).
+        //    Where(cid => cid.CustomerId == id);
+        //    return listOfBookingDetails.ToList();
+        //}
+
+    }
+}
