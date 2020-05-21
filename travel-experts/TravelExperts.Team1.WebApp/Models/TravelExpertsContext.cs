3cf5c1496271e6cc25cb2fcf3246a1d5553c8d51
LidaGold <50148896+LidaGold@users.noreply.github.com>
2020-05-20 20:30:30 -0600

    Formatted packages page

--- travel-experts/TravelExperts.Team1.WebApp/Models/TravelExpertsContext.cs
+++ travel-experts/TravelExperts.Team1.WebApp/Models/TravelExpertsContext.cs
@@ -41,7 +41,7 @@ namespace TravelExperts.Team1.WebApp.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=localhost\\SQLExpress;Initial Catalog=TravelExperts_Team1;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=TravelExperts_Team1;Integrated Security=True");
             }
         }
 
